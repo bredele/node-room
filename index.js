aca65fce21bd534eed50bae646317e6916035254
bredele <olivier.wietrich@gmail.com>
2014-05-24 17:12:14 -0600

    initialize package

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/**
+ * Expose 'Room'
+ */
+
+module.exports = Room;
+
+
+/**
+ * Room constructor.
+ * @api public
+ */
+
+function Room() {
+  //do something
+}
---
+++ test/room.js
@@ -0,0 +1,21 @@
+
+/**
+ * Test dependencies.
+ */
+
+var assert = require('assert');
+var room = require('..');
+
+describe("API", function() {
+	
+	var chat;
+	beforeEach(function() {
+		chat = room('chat');
+	});
+	
+	it("should create a room", function() {
+		
+	});
+	
+
+});
